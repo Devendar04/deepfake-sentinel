e529e8d95172a44d5f6f2ffa6312c444484a253a
Dev <kristinbond142@gmail.com>
2026-03-29 10:16:03 +0530

    hugging face

--- backend/main.py
+++ backend/main.py
@@ -21,8 +21,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173",
         "https://deepfake-sentinel.vercel.app",
+        "https://devendra174-deepfake-sentinel.hf.space",
     ],
     allow_credentials=True,
     allow_methods=["*"],
